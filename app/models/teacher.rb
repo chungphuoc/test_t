a53a072080b5f41b3ca34f085fe447a0a19dfe9c
phamhoaivu <phamhoaivu911@gmail.com>
2016-05-05 15:35:42 +0700

    validate teachers

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,6 @@ class Teacher < ActiveRecord::Base
   has_many :courses
   has_many :contracts, dependent: :destroy
   has_many :studio, through: :contracts
+
+  validates :name, presence: true, uniqueness: true
 end
