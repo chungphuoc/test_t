40cd873b42c5712ab774b29613f0cdccfa7fd705
phamhoaivu <phamhoaivu911@gmail.com>
2016-05-05 13:48:32 +0700

    validates courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,4 +5,13 @@ class Course < ActiveRecord::Base
   belongs_to :studio
   has_many :enrollments, dependent: :destroy
   has_many :customers, through: :enrollments
+
+  validates :name, presence: true
+  validates :phone_number, presence: true
+  validates :num_slot, presence: true
+  validates :start_date, presence: true
+  validates :teacher, presence: true
+  validates :station, presence: true
+  validates :exercise, presence: true
+  validates :website, format: { with: /\A^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix }, allow_blank: true
 end
