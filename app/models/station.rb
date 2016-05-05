bc798c6919fb31846de69e14d730e5d7700a9500
phamhoaivu <phamhoaivu911@gmail.com>
2016-05-05 16:03:38 +0700

    validate station

--- app/models/station.rb
+++ app/models/station.rb
@@ -2,4 +2,6 @@ class Station < ActiveRecord::Base
   has_many :courses
   has_many :branches, dependent: :destroy
   has_many :studios, through: :branches
+
+  validates :location, presence: true, uniqueness: true
 end
