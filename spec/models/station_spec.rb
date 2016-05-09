c64bdb8f9b18c239dff2a58ade97b4532526f318
phamhoaivu <phamhoaivu911@gmail.com>
2016-05-09 16:53:35 +0700

    write spec for station model

---
+++ spec/models/station_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Station, type: :model do
+  it { should validate_presence_of :location }
+  it { should validate_uniqueness_of :location }
+  it { should have_many :courses }
+  it { should have_many :branches }
+  it { should have_many :studios }
+end
