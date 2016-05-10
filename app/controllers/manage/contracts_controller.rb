9414a4b1831515ab5fe8a63fc9ac09896b5ff46f
phamhoaivu <phamhoaivu911@gmail.com>
2016-05-10 22:10:17 +0700

    studio add teachers

---
+++ app/controllers/manage/contracts_controller.rb
@@ -0,0 +1,34 @@
+class Manage::ContractsController < Manage::BaseController
+
+  def index
+    @contracts = @studio.contracts.includes(:teacher)
+  end
+
+  def new
+    @contract = Contract.new
+    @contract.build_teacher
+  end
+
+  def create
+    @contract = @studio.contracts.new(contract_params)
+    if @contract.save
+      flash[:success] = 'Teacher has been successfully added.'
+      redirect_to manage_contracts_url
+    else
+      flash.now[:error] = 'Error!'
+      render :new
+    end
+  end
+
+  def destroy
+    @contract = Contract.find(params[:id])
+    @contract.destroy
+    flash[:success] = 'Teacher has been successfully removed.'
+    redirect_to manage_contracts_url
+  end
+
+  private
+    def contract_params
+      params.require(:contract).permit(teacher_attributes: [:name])
+    end
+end
--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,4 +1,14 @@
 class Contract < ActiveRecord::Base
   belongs_to :teacher
   belongs_to :studio
+
+  delegate :name, to: :teacher, prefix: true
+  accepts_nested_attributes_for :teacher
+  validates_associated :teacher
+  validates :teacher, uniqueness: true
+  before_validation :find_teacher
+
+  def find_teacher
+    self.teacher = Teacher.where(name: teacher.name).first_or_initialize
+  end
 end
