a15fc1ac6741d1130d51ca97f12073e09f172f4d
nguyenductoan <ductoan593@gmail.com>
2016-05-18 11:18:49 +0700

    remove useles code

--- app/controllers/personal/favourite_courses_controller.rb
+++ app/controllers/personal/favourite_courses_controller.rb
@@ -4,7 +4,7 @@ class Personal::FavouriteCoursesController < Personal::BaseController
 
   def index
     @courses = CoursesQuery.new(current_user).favourite_courses
-    @courses = @courses.paginate(:page => params[:page], :per_page => 9)
+                           .paginate(:page => params[:page], :per_page => 9)
   end
 
   def add
