f46547545269e148499a04c26e3d116456cb79b3
nguyenductoan <ductoan593@gmail.com>
2016-05-23 15:48:30 +0700

    invite friend by email

---
+++ app/mailers/invite_friend_mailer.rb
@@ -0,0 +1,10 @@
+class InviteFriendMailer < ActionMailer::Base
+  default from: 'admin@tone.com'
+  def to_friend(user, friend_name, friend_mail)
+    @user = user
+    @friend_mail = friend_mail
+    @friend_name = friend_name
+    subject = 'Invitation to Tone'
+    mail(to: friend_mail, subject: subject, template_name: 'to_friend')
+  end
+end
