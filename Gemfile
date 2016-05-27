f6f03ceae894696d69894f723f5d7bb797159fa7
nguyenductoan <ductoan593@gmail.com>
2016-05-27 16:33:41 +0700

    upload image to amazon S3

--- Gemfile
+++ Gemfile
@@ -31,6 +31,8 @@ gem 'stripe'
 
 gem 'whenever', :require => false
 
+gem 'fog'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,19 @@
+if Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.storage = :file
+  end
+else
+  CarrierWave.configure do |config|
+
+    config.storage = :fog
+
+    config.fog_credentials = {
+        :provider               => 'AWS',
+        :aws_access_key_id      => ENV['S3_KEY'],
+        :aws_secret_access_key  => ENV['S3_SECRET'],
+        :region                 => ENV['S3_REGION']
+    }
+
+    config.fog_directory  = ENV['S3_BUCKET']
+  end
+end
