c872cdf2523be50e5ceb7aea675a10e00eb2a3e9
phamhoaivu <phamhoaivu911@gmail.com>
2016-06-02 13:52:20 +0700

    courses_search_service_spec

---
+++ spec/factories/courses.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :course do
+    name
+    phone_number '0123456789'
+    num_slot 10
+    start_date Date.current
+    association :teacher, factory: :teacher
+    association :station, factory: :station
+    association :exercise, factory: :exercise
+    website Faker::Internet.url
+  end
+end
--- spec/services/studio_exercise_service_spec.rb
+++ spec/services/studio_exercise_service_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe StudioExerciseService, type: :service do
   let(:exercise_1) { create(:exercise) }
   let(:exercise_2) { create(:exercise) }
   let(:exercise_3) { create(:exercise) }
+  let(:studio_exercise_service) { StudioExerciseService.new(studio) }
 
   context 'Studio add new exercises to its exercises list' do
-    before { StudioExerciseService.new(studio).update_exercises([exercise_1.id]) }
+    before { studio_exercise_service.update_exercises([exercise_1.id]) }
     it { expect(studio.exercises.pluck(:id)).to eq([exercise_1.id]) }
   end
 
   context 'Studio remove old exercises from its exercises list' do
     before do
-      studio_exercise_service = StudioExerciseService.new(studio)
       studio_exercise_service.update_exercises([exercise_1.id, exercise_2.id])
       studio_exercise_service.update_exercises([exercise_2.id])
     end
@@ -23,7 +23,6 @@ RSpec.describe StudioExerciseService, type: :service do
 
   context 'Studio add new exercises to and remove old exercises from its exercises list' do
     before do
-      studio_exercise_service = StudioExerciseService.new(studio)
       studio_exercise_service.update_exercises([exercise_1.id, exercise_2.id])
       studio_exercise_service.update_exercises([exercise_2.id, exercise_3.id])
     end
