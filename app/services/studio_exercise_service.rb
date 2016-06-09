f7c64180ed7e77d49fb7a52e012c6e792811ab87
nguyenductoan <ductoan593@gmail.com>
2016-06-09 11:49:25 +0700

    fixed error update exercise

--- app/services/studio_exercise_service.rb
+++ app/services/studio_exercise_service.rb
@@ -4,6 +4,7 @@ class StudioExerciseService
   end
 
   def update_exercises(exercise_ids)
+    exercise_ids = exercise_ids.collect{|s| s.to_i}
     destroyed_exercises = @studio.exercises.where.not(id: exercise_ids)
     new_exercises = Exercise.where(id: (exercise_ids - @studio.exercises.pluck(:id)))
     @studio.exercises.destroy(destroyed_exercises)
