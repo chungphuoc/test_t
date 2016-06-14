b5ed6d30d234493dfd6cc7b179455428131b77a0
nguyenductoan <ductoan593@gmail.com>
2016-06-14 10:48:54 +0700

    add validate to enrollment, do not display closed course

--- app/controllers/personal/courses_controller.rb
+++ app/controllers/personal/courses_controller.rb
@@ -1,10 +1,14 @@
 class Personal::CoursesController < Personal::BaseController
   def index
-    @courses = Course.page(params[:page])
+    @courses = Course.where(status: Course.statuses[:active]).page(params[:page])
   end
 
   def show
     @course = Course.find(params[:id])
+    if @course.inactive?
+      flash[:notice] = 'This class is not open.'
+      redirect_to :back
+    end
   end
 
   def search
