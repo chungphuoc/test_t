abbb5c7b3ce32056c5a1f3f4a866a5f14336600e
nguyenductoan <ductoan593@gmail.com>
2016-06-24 16:57:03 +0700

    add globalize gem, add station_translate, add traslate to static text navigation studio

--- app/models/station.rb
+++ app/models/station.rb
@@ -3,12 +3,20 @@ class Station < ActiveRecord::Base
   has_many :courses
   has_many :branches, dependent: :destroy
   has_many :studios, through: :branches
+  has_many :translations
   belongs_to :requester, class_name: 'Studio'
 
   validates :location, presence: true, uniqueness: true
-  validates :name, presence: true, uniqueness: true
+  #validates :name, presence: true, uniqueness: true
   validates :latitude, presence: true
   validates :longitude, presence: true
+
+  accepts_nested_attributes_for :translations
+  translates :name, :string
+
+  class Translation
+    validates :name, presence: true
+  end
 
   def name_with_initial
     name
