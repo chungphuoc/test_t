bba361d45218cb18ba6177facabcd03e86f232e8
nguyenductoan <ductoan593@gmail.com>
2016-06-30 17:57:47 +0700

    fixed bug update days_of_week admin page

--- app/assets/javascripts/admin/course.js
+++ app/assets/javascripts/admin/course.js
@@ -1,5 +1,5 @@
 function change_studio_option(){
-  studio_id = $("#studio_studio_id").find(":selected").val();
+  studio_id = $("#course_studio_id").find(":selected").val();
   $.ajax({
     url: "/admin/courses/change_studio",
     data: {studio_id: studio_id}
