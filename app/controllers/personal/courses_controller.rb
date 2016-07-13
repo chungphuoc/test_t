bb5f688ca51aa7d03eb75ec708c303254308c8cf
Phuoc <onhachungphuoc@gmail.com>
2016-07-13 14:42:48 +0700

    finish implement calendar

--- app/controllers/personal/courses_controller.rb
+++ app/controllers/personal/courses_controller.rb
@@ -1,4 +1,5 @@
 class Personal::CoursesController < Personal::BaseController
+  include ActionView::Helpers::NumberHelper
   # layout false, only: ['personal']
   layout 'personal_no_panel'
 
@@ -16,17 +17,40 @@ class Personal::CoursesController < Personal::BaseController
   end
 
   def search
-    @courses = CoursesSearchService.new(params).execute.page(params[:page])
-    @courses = @courses.collect do |course|
-      {
-        id: course.id,
-        title: course.name,
-        url: "http://www.example.com/",#personal_course_url(course),
-        class: 'event-important',
-        start: DateTime.now.to_f#(course.start_date.to_datetime + course.start_time.seconds_since_midnight.seconds).to_f * 1000
-      }
+    respond_to do |format|
+      @favourite_courses = current_user.favourite_courses.includes(:course)
+      @courses = CoursesSearchService.new(params).execute.page(params[:page])
+      events_class = %w(event-warning event-important event-info event-special)
+      @courses = @courses.collect do |course|
+        start_date = convert_time(course.start_date, course.start_time)
+        title = "<b>#{course.name}</b> <br> #{course.teacher.name}
+          <br> #{course.studio.name} <br> #{course.station.name}
+          <br> #{number_with_delimiter(course.tuition)} #{course.currency}".html_safe
+        {
+          id: course.id,
+          title: title,
+          url: personal_course_path(course),
+          class: events_class.sample,
+          start: start_date,
+          end: start_date.to_i + 2.hours.in_milliseconds
+        }
+      end
+      @result = { success: '1', result: @courses }.to_json
+      format.html { render :search_result, layout: 'personal_background' }
+      format.json { render json: @result }
     end
-    @result = {success: '1', result: @courses}
-    render :search_result, layout: 'personal_background'
   end
+
+  private
+    def convert_time(start_date, start_time)
+      dt = DateTime.new(
+        start_date.year,
+        start_date.month,
+        start_date.day,
+        start_time.hour,
+        start_time.min,
+        start_time.sec
+      )
+      dt.strftime('%Q')
+    end
 end
