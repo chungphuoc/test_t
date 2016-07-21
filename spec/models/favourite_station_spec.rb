cecabc7cf3d0448fe39eec13dbab1a3a23acea58
An Luu <lta.anluu@gmail.com>
2016-07-21 11:28:53 +0700

    rspec

--- spec/models/favourite_station_spec.rb
+++ spec/models/favourite_station_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe FavouriteStation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :customer }
+  it { should belong_to :station }
 end
