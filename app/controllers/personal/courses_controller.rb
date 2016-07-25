af5d1cb1f771ea280b36c90833d0b9d5ba21df51
An Luu <lta.anluu@gmail.com>
2016-07-25 14:00:32 +0700

    change template calendar day

--- app/controllers/personal/courses_controller.rb
+++ app/controllers/personal/courses_controller.rb
@@ -27,6 +27,7 @@ class Personal::CoursesController < Personal::BaseController
         {
           id: course.id,
           title: title,
+          tmpls_day: template_day(course),
           name: course.name,
           url: personal_course_path(course),
           class: events_class.sample,
@@ -77,6 +78,27 @@ class Personal::CoursesController < Personal::BaseController
       "<br><i>Studio: #{course.studio.name}</i>" +
       "<br><i>Station: #{course.station.name}</i>" +
       "<br><i>Tuition: #{number_with_delimiter(course.tuition)} #{course.currency}</i>" +
+      "</div></div>".html_safe
+    end
+
+    def template_day(course)
+      "<div class='course-calendar-day'>" +
+        "<img src='#{course.cover_img}'>" +
+        "<div class='info-course'>" +
+          "<div class='course-title'>" +
+            "<b>#{course.name}</b>" +
+          "</div>" +
+          "<div class='row'>" +
+            "<div class='col-xs-6'>" +
+              "<p>#{course.studio.name}</p>" +
+              "<p>#{course.teacher.name}</p>" +
+              "<p>#{course.station.name}</p>" +
+            "</div>" +
+            "<div class='col-xs-6'>" +
+              "<p>#{course.kcal} kcal</p>" +
+              "<p>#{course.tuition} usd</p>" +
+            "</div>" +
+          "</div>" +
       "</div></div>".html_safe
     end
 end
