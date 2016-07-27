7c4109cf72ca6d30ac6b0373928a538a42b45cd5
An Luu <lta.anluu@gmail.com>
2016-07-27 10:22:10 +0700

    add slider for time

--- app/controllers/personal/courses_controller.rb
+++ app/controllers/personal/courses_controller.rb
@@ -21,6 +21,7 @@ class Personal::CoursesController < Personal::BaseController
       @favourite_courses = current_user.favourite_courses.includes(:course)
       @courses = CoursesSearchService.new(params).execute.page(params[:page])
       @kcal_courses = @courses.map{ |course| course.kcal }
+      @time_courses = @courses.map{ |course| course.start_time}
       events_class = %w(event-warning event-important event-info event-special)
       @courses = @courses.collect do |course|
         start_date = convert_time(course.start_date, course.start_time)
