b6c377763b0ecc11496d8dd4250f26343b97a480
An Luu <lta.anluu@gmail.com>
2016-07-28 17:08:47 +0700

    add validates

--- app/models/featured_course.rb
+++ app/models/featured_course.rb
@@ -1,6 +1,8 @@
 class FeaturedCourse < ActiveRecord::Base
   belongs_to :course
 
+  validates :course_id, presence: true, uniqueness: true
+
   def self.added?(course_id)
     FeaturedCourse.all.exists?(course_id: course_id)
   end
