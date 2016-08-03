5467d8513b5570333cb4dc658c32c55df829a460
fanNPA <phianhnpa1995@gmail.com>
2016-08-03 16:56:02 +0700

    rubocop

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -6,15 +6,18 @@ class FeedbackMailer < ActionMailer::Base
     @email = email
     @message = message
     subject = 'Feedback from user.'
-    mail(to: Rails.application.secrets.admin_email, subject: subject, template_name: 'to_admin')
+    mail(to: Rails.application.secrets.admin_email,
+         subject: subject,
+         template_name: 'to_admin',
+         content_type: 'multipart/alternative')
   end
 
   def from_customer_to_studio(customer, course, message)
-  	@customer = customer
-  	@studio = course.studio
-  	@course = course
-  	@message = message
-  	subject = "Feedback from customer #{@customer.name}"
-  	mail(to: @studio.email, subject: subject, template_name: 'to_studio')
+    @customer = customer
+    @studio = course.studio
+    @course = course
+    @message = message
+    subject = "Feedback from customer #{@customer.name}"
+    mail(to: @studio.email, subject: subject, template_name: 'to_studio')
   end
 end
