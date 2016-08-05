8f0cc643e78f2d0e2ad435bd65ae61fe9fb6ef44
An Luu <lta.anluu@gmail.com>
2016-08-05 10:58:20 +0700

    fix search by time and calories

--- app/controllers/personal/courses_controller.rb
+++ app/controllers/personal/courses_controller.rb
@@ -20,8 +20,6 @@ class Personal::CoursesController < Personal::BaseController
     respond_to do |format|
       @favourite_courses = current_user.favourite_courses.includes(:course)
       @courses = CoursesSearchService.new(params).execute.page(params[:page])
-      @kcal_courses = @courses.map(&:kcal)
-      @time_courses = @courses.map(&:start_time)
       events_class = %w(event-warning event-important event-info event-special)
       @courses = @courses.collect do |course|
         start_date = convert_time(course.start_date, course.start_time)
