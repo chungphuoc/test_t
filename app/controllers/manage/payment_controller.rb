cdb7c01c17e2d1756461681a8152cd5a3dfa2f4f
An Luu <lta.anluu@gmail.com>
2016-08-08 11:49:55 +0700

    add total price

--- app/controllers/manage/payment_controller.rb
+++ app/controllers/manage/payment_controller.rb
@@ -2,11 +2,13 @@ class Manage::PaymentController < Manage::BaseController
   def index
     @enrollments = Enrollment.includes(course: [ :studio ])
                             .page(params[:page])
+    @total_payment = cal_total_payment(@enrollments)
   end
 
   def year_filter
     @year = params[:year]
     @enrollments = Enrollment.where("extract(year from created_at) =?", @year).page(params[:page])
+    @total_payment = cal_total_payment(@enrollments)
     render 'payment_table', layout: false
   end
 
@@ -15,9 +17,17 @@ class Manage::PaymentController < Manage::BaseController
     @month = params[:month]
     @enrollments = if @month.to_i == 0
                       Enrollment.where("extract(year from created_at) =?", @year).page(params[:page])
-                    else 
+                    else
                       Enrollment.where("extract(year from created_at) =? AND extract(month from created_at) =?", @year, @month).page(params[:page])
                     end
+    @total_payment = cal_total_payment(@enrollments)
     render 'payment_table', layout: false
+  end
+
+  private
+
+  def cal_total_payment(enrollments)
+    return 0 if enrollments.nil?
+    enrollments.map { |enrollment| enrollment.course.tuition }.inject(0, :+)
   end
 end
