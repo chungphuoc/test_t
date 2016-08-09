1d403b7d813841d7a4652d12634ef896700b4345
An Luu <lta.anluu@gmail.com>
2016-08-09 11:31:46 +0700

    create payable option page

--- app/controllers/manage/payable_options_controller.rb
+++ app/controllers/manage/payable_options_controller.rb
@@ -6,11 +6,21 @@ class Manage::PayableOptionsController < Manage::BaseController
   def create
     @payable_option = PayableOption.new(payable_option_params)
     if @payable_option.save
-      set_flash_message :success, :create
+      set_flash_message :success, :created
       redirect_to manage_payable_options_path
     else
       render :index
     end
+  end
+
+  def destroy
+    option = @studio.options.find(params[:id])
+    if option.destroy
+      set_flash_message :success, :destroyed
+    else
+      set_flash_message :error, :destroy_failed
+    end
+    redirect_to manage_payable_options_path
   end
 
   private
--- app/models/payable_option.rb
+++ app/models/payable_option.rb
@@ -1,19 +1,19 @@
 class PayableOption < ActiveRecord::Base
-	before_destroy :check_for_payments
+  before_destroy :check_for_payments
   before_save :downcase_name
-	belongs_to :studio
-	has_many :option_enrollments, foreign_key: :option_id
-	has_many :enrollments, through: :option_enrollments
+  belongs_to :studio
+  has_many :option_enrollments, foreign_key: :option_id
+  has_many :enrollments, through: :option_enrollments
 
-	validates :name, presence: true, uniqueness: { case_sensitive: false, scope: [:studio_id] }
-	validates :price, presence: true
+  validates :name, presence: true, uniqueness: { case_sensitive: false, scope: [:studio_id] }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   def check_for_payments
-  	if enrollments.any?
-  		errors.add('Cannot delete option that has already been paid')
-  		return false
-  	end
-  	return true
+    if enrollments.any?
+      errors.add('Cannot delete option that has already been paid')
+      false
+    end
+    true
   end
 
   def downcase_name
