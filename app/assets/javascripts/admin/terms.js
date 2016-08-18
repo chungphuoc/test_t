9d67cdc7abfce1111fbfe0a83efcbe5009412e82
An Luu <lta.anluu@gmail.com>
2016-08-18 17:43:32 +0700

    term-condition in admin page

--- app/assets/javascripts/admin/terms.js
+++ app/assets/javascripts/admin/terms.js
@@ -4,3 +4,17 @@ CKEDITOR.editorConfig = function (config) {
   ];
   config.toolbar = "simple";
 }
+
+function show_term_customer(button){
+  var customer = document.getElementById('term_customer');
+  var studio = document.getElementById('term_studio');
+  customer.style.display = 'block';
+  studio.style.display = 'none';
+}
+
+function show_term_studio() {
+  var customer = document.getElementById('term_customer');
+  var studio = document.getElementById('term_studio');
+  customer.style.display = 'none';
+  studio.style.display = 'block';
+}
