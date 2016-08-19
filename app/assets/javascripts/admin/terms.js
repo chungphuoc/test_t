82d66b7abc96c378cced4cc87381f16d50f2e9ea
An Luu <lta.anluu@gmail.com>
2016-08-19 10:01:25 +0700

    fix ui term-condition admin page

--- app/assets/javascripts/admin/terms.js
+++ app/assets/javascripts/admin/terms.js
@@ -5,16 +5,28 @@ CKEDITOR.editorConfig = function (config) {
   config.toolbar = "simple";
 }
 
-function show_term_customer(button){
-  var customer = document.getElementById('term_customer');
-  var studio = document.getElementById('term_studio');
-  customer.style.display = 'block';
-  studio.style.display = 'none';
+const CUSTOMER = 'customer';
+const STUDIO = 'studio';
+
+function clear_active_button() {
+  var button = document.querySelector('#admin_term_index .list-button .active');
+  button.classList.remove('active');
 }
 
-function show_term_studio() {
+function show_term(user_type = CUSTOMER){
   var customer = document.getElementById('term_customer');
   var studio = document.getElementById('term_studio');
-  customer.style.display = 'none';
-  studio.style.display = 'block';
+  var button = document.getElementById('button_term_' + user_type);
+  clear_active_button();
+  button.classList.add('active');
+  if (user_type === CUSTOMER) {
+    toggle_term_condition(customer, studio);
+  } else {
+    toggle_term_condition(studio, customer);
+  }
+}
+
+function toggle_term_condition(term_show, term_hide) {
+  term_show.style.display = 'block';
+  term_hide.style.display = 'none';
 }
